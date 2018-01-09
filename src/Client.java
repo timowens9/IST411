77233ce1a2ef698123453fcfc465067a6d97053d
Tim Owens <timowens9@gmail.com>
2018-01-09 17:06:46 -0600

    Add files via upload
    added client/ server classes

---
+++ src/Client.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String args[]) {
+
+        try {
+            System.out.println(" Client has be started");
+            Socket soc = new Socket("localhost", 1024);
+            BufferedReader inputString = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter your Message");
+            String str = inputString.readLine();
+            PrintWriter output = new PrintWriter(soc.getOutputStream(),true);
+            output.println(str);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            System.out.println(in.readLine());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+
+        }
+
+
+
+    }
+}
